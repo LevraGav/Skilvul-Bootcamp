415f55869fed2d113c661c0a2dfab9b5cdce9e09
Arvel Gavrilla R <arvel.gavrilla@gmail.com>
2021-09-28 15:26:03 +0700

    tdying up

--- 6. JavaScript Dasar/02-operator/02-operator.js
+++ 6. JavaScript Dasar/02-operator/02-operator.js
@@ -34,7 +34,6 @@ if (ribuan) {
 
 console.log(rupiah);
 
-
 /// Soal - 02
 /// Gunakan operator yang tepat untuk mengetahui apakah 2 variable dibawah ini memiliki nilai yang sama (tipe data tidak termasuk)
 const oneInString = "1";
