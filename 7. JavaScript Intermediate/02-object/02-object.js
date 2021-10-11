2733518bb07e0d9d32792de2e6f278608eb4c608
Arvel Gavrilla R <arvel.gavrilla@gmail.com>
2021-10-11 08:33:34 +0700

    adding number 2

--- 7. JavaScript Intermediate/02-object/02-object.js
+++ 7. JavaScript Intermediate/02-object/02-object.js
@@ -64,4 +64,50 @@ const isMilkVegan = getObjectValue(milkBasedCoffee, "ingredients.milk.isVegan");
 console.log("\nNumber 2");
 console.log(espresso);
 console.log(coffeeBrand);
-console.log(isMilkVegan)
+console.log(isMilkVegan);
+
+//No 3
+const items = [
+    {
+        btc: { buy: 10, sell: 9 },
+        eth: { buy: 8, sell: 7.5 },
+        doge: { buy: 7, sell: 6.5 },
+        day: 1,
+    },
+
+    {
+        btc: { buy: 9, sell: 5 },
+        eth: { buy: 7, sell: 4 },
+        doge: { buy: 6, sell: 3 },
+        day: 2,
+    },
+
+    {
+        btc: { buy: 5, sell: 10 },
+        eth: { buy: 4, sell: 6 },
+        doge: { buy: 3, sell: 4 },
+        day: 3,
+    },
+];
+
+const calculateIncome = (items) => {
+    return items.reduce((acc, curr) => {
+        const btcSum = curr.btc.sell - curr.btc.buy;
+        const dogeSum = curr.doge.sell - curr.doge.buy;
+        const ethSum = curr.eth.sell - curr.eth.buy;
+        return {
+            btc: acc.btc + btcSum,
+            doge: acc.doge + dogeSum,
+            eth: acc.eth + ethSum,
+        };
+    }, 
+    
+    {
+        btc: 0,
+        doge: 0,
+        eth: 0
+    });
+}
+
+console.log("\nNumber 3");
+console.log(calculateIncome(items));
